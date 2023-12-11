423ba5c6bc64fac23ebfeb3bba1bea157c2a55d1
Hokyun <h0kyungim@gmail.com>
2023-12-11 23:40:49 +0900

    feat: GreetingText 파일에 컬럼과 추가 텍스트 추가
    - Jetpack Compose를 이용한 개발을 연습해봄

--- app/src/main/java/com/example/tutorialapplication/MainActivity.kt
+++ app/src/main/java/com/example/tutorialapplication/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.tutorialapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.tutorialapplication.ui.theme.TutorialApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +25,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             TutorialApplicationTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingText("Happy Birthday Yeun!", "Hokyun")
                 }
             }
         }
@@ -27,17 +37,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "From. $from",
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun BirthdayCardPreview() {
     TutorialApplicationTheme {
-        Greeting("Test")
+        GreetingText("Happy Birthday A!", "Hokyun")
     }
 }
