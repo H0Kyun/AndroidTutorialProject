ed4108d43f29443ac246700220e657d402911e9c
Hokyun <h0kyungim@gmail.com>
2024-04-02 21:23:12 +0900

    remove: 불필요한 파일 삭제

--- app/src/main/java/com/example/tutorialapplication/chapter1/Base.kt
+++
@@ -1,9 +0,0 @@
-package com.example.tutorialapplication.chapter1
-
-open class Base(open val mutableProperty: Int) {
-
-    open val propertyForOverride = 4
-    open fun canOverride() {}
-    open fun canOverrideAndNotFinal() {}
-    fun canNotOverride() {}
-}
