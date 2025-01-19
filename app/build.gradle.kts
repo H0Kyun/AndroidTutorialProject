5728c3a3d0c6e46da830e8e3943a3601c6752763
H0kyun <h0kyungim@gmail.com>
2025-01-19 13:40:55 +0900

    feat: 안 쓴느 파일 삭제

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,8 +50,8 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
